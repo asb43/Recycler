c4caac4d0641952bc08828e9b8d21c4f3dd35c2f
asb43 <pokemonfanboy600@gmail.com>
2022-11-08 19:32:25 -0800

    finished with recycler view stuff

--- app/src/main/java/com/zybooks/affirmations/MainActivity.kt
+++ app/src/main/java/com/zybooks/affirmations/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.zybooks.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.zybooks.affirmations.adapter.ItemAdapter
+import com.zybooks.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadaffirmation()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_widget)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
